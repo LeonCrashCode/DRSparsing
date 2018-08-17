4f6897088fb156fd904139e64980082a98a5c9bb
Jiangming Liu <jiangming_liu@zhaoziyudeMacBook.local>
2018-08-17 18:41:41 +0100

    init

--- lambdaExpr/tc.py
+++ lambdaExpr/tc.py
@@ -7,7 +7,8 @@ def ascii_encode_dict(data):
     ascii_encode = lambda x: x.encode('utf-8') if isinstance(x, unicode) else x 
     return dict(map(ascii_encode, pair) for pair in data.items())
 
-def tc1(node): #N->NP, merge
+def tc1(node, start): #N->NP, merge
+	v = start + "0"
 
 	node_app = DRSnode()
 	node_app.type = "app"
@@ -16,15 +17,15 @@ def tc1(node): #N->NP, merge
 	node_var1.text = "v1"
 	node_var2 = DRSnode()
 	node_var2.type = "var"
-	node_var2.text = "x0"
+	node_var2.text = v
 	node_app.expression.append(node_var1)
 	node_app.expression.append(node_var2)
 
 	node_merge = DRSnode()
 	node_merge.type = "merge"
 
-	node.modify_attrib("v1", "x0")
-	node.add_variable("x0", True)
+	node.modify_attrib("v1", v)
+	node.add_variable(v, True)
 
 	node_merge.expression.append(node.expression[1])
 	node_merge.expression.append(node_app)
@@ -34,7 +35,8 @@ def tc1(node): #N->NP, merge
 
 	return node
 
-def tc2(node): #N->NP. alfa def
+def tc2(node, start): #N->NP. alfa def
+	v = start + "0"
 
 	node_app = DRSnode()
 	node_app.type = "app"
@@ -43,7 +45,7 @@ def tc2(node): #N->NP. alfa def
 	node_var1.text = "v1"
 	node_var2 = DRSnode()
 	node_var2.type = "var"
-	node_var2.text = "x0"
+	node_var2.text = v
 	node_app.expression.append(node_var1)
 	node_app.expression.append(node_var2)
 
@@ -51,8 +53,8 @@ def tc2(node): #N->NP. alfa def
 	node_merge.type = "alfa"
 	node_merge.attrib = {"type": "def"}
 
-	node.modify_attrib("v1", "x0")
-	node.add_variable("x0", True)
+	node.modify_attrib("v1", v)
+	node.add_variable(v, True)
 	
 	node_merge.expression.append(node.expression[1])
 	node_merge.expression.append(node_app)
@@ -61,33 +63,6 @@ def tc2(node): #N->NP. alfa def
 	normal_variables(node)
 	return node
 
-def tc2_s(node): #N->NP. alfa def
-
-	node_app = DRSnode()
-	node_app.type = "app"
-	node_var1 = DRSnode()
-	node_var1.type = "var"
-	node_var1.text = "v1"
-	node_var2 = DRSnode()
-	node_var2.type = "var"
-	node_var2.text = "s0"
-	node_app.expression.append(node_var1)
-	node_app.expression.append(node_var2)
-
-	node_merge = DRSnode()
-	node_merge.type = "alfa"
-	node_merge.attrib = {"type": "def"}
-
-	node.modify_attrib("v1", "s0")
-	node.add_variable("s0", True)
-	
-	node_merge.expression.append(node.expression[1])
-	node_merge.expression.append(node_app)
-	node.expression[1] = node_merge
-
-	normal_variables(node)
-
-	return node
 
 
 if __name__ == "__main__":
@@ -107,7 +82,7 @@ if __name__ == "__main__":
 			source = json.loads(L[5], object_hook=ascii_encode_dict)
 			source_DRSnode = DRSnode()
 			source_DRSnode.unserialization(source)
-			source_DRSnode = tc1(source_DRSnode)
+			source_DRSnode = tc1(source_DRSnode, "x")
 			change = json.dumps(source_DRSnode.serialization())
 
 			if target == change:
@@ -118,7 +93,7 @@ if __name__ == "__main__":
 			source = json.loads(L[5], object_hook=ascii_encode_dict)
 			source_DRSnode = DRSnode()
 			source_DRSnode.unserialization(source)
-			source_DRSnode = tc2(source_DRSnode)
+			source_DRSnode = tc1(source_DRSnode, "s")
 			change = json.dumps(source_DRSnode.serialization())
 
 			if target == change:
@@ -129,7 +104,18 @@ if __name__ == "__main__":
 			source = json.loads(L[5], object_hook=ascii_encode_dict)
 			source_DRSnode = DRSnode()
 			source_DRSnode.unserialization(source)
-			source_DRSnode = tc2_s(source_DRSnode)
+			source_DRSnode = tc2(source_DRSnode, "x")
+			change = json.dumps(source_DRSnode.serialization())
+
+			if target == change:
+				eq += 1
+				L = []
+				continue
+
+			source = json.loads(L[5], object_hook=ascii_encode_dict)
+			source_DRSnode = DRSnode()
+			source_DRSnode.unserialization(source)
+			source_DRSnode = tc2(source_DRSnode, "s")
 			change = json.dumps(source_DRSnode.serialization())
 
 			if target == change:
