aad0358a3cc3ee43e9f2608ae05ec79b941a4200
Jiangming Liu <jmliunlp@gmail.com>
2018-09-09 15:25:54 +0100

    init

---
+++ Tree/drs2tree.py
@@ -0,0 +1,120 @@
+import sys
+import types
+import json
+import re
+from defination import DRSnode
+from utils import normal_variables_for_trees
+def ascii_encode_dict(data):
+    ascii_encode = lambda x: x.encode('utf-8') if isinstance(x, unicode) else x 
+    return dict(map(ascii_encode, pair) for pair in data.items())
+
+def tree(node):
+	
+	tree = []
+	def travel(n):
+		if n.type == "drs":
+			tree.append("DRS(")
+			assert len(n.expression) == 2
+			travel(n.expression[1])
+			tree.append(")")
+		elif n.type == "sdrs":
+			tree.append("SDRS(")
+			assert len(n.expression) == 2
+			travel(n.expression[0])
+			travel(n.expression[1])
+			tree.append(")")
+		elif n.type == "sub":
+			for sn in n.expression:
+				travel(sn)
+		elif n.type == "constituents":
+			for sn in n.expression:
+				travel(sn)
+		elif n.type == "constituent":
+			tree.append(n.attrib["label"].upper()+"(")
+			assert len(n.expression) == 1
+			travel(n.expression[0])
+			tree.append(")")
+		elif n.type == "relations":
+			for sn in n.expression:
+				tree.append(sn.attrib["sym"]+"(")
+				tree.append(sn.attrib["arg1"].upper())
+				tree.append(sn.attrib["arg2"].upper())
+				tree.append(")")
+		elif n.type == "conds":
+			for sn in n.expression:
+				travel(sn)
+		elif n.type == "cond":
+			assert len(n.expression) == 1 # cond
+			sn = n.expression[0]
+			if sn.type in ["rel"]:
+				tree.append(sn.attrib["symbol"]+"(")
+				tree.append(sn.attrib["arg1"].upper())
+				tree.append(sn.attrib["arg2"].upper())
+			elif sn.type == "eq":
+				tree.append("EQ(")
+				tree.append(sn.attrib["arg1"].upper())
+				tree.append(sn.attrib["arg2"].upper())
+			elif sn.type in ["named", "pred"]:
+				tree.append(sn.attrib["symbol"]+"(")
+				tree.append(sn.attrib["arg"].upper())
+			elif sn.type == "card":
+				tree.append("CARD(")
+				tree.append(sn.attrib["arg"].upper())
+				tree.append("CARD_NUMBER")
+			elif sn.type == "timex":
+				tree.append("TIMEX(")
+				tree.append(sn.attrib["arg"].upper())
+				tree.append("TIME_NUMBER")
+			elif sn.type == "prop":
+				tree.append(sn.attrib["argument"].upper()+"(")
+				assert len(sn.expression) == 1
+				travel(sn.expression[0])
+			elif sn.type in ["pos", "nec", "not"]:
+				tree.append(sn.type.upper()+"(")
+				assert len(sn.expression) == 1
+				travel(sn.expression[0])
+			elif sn.type in ["imp", "or", "duplex"]:
+				tree.append(sn.type.upper()+"(")
+				assert len(sn.expression) == 2
+				travel(sn.expression[0])
+				travel(sn.expression[1])
+			else:
+				print sn.type
+				assert False, "unrecognized type"
+			tree.append(")")
+	travel(node)
+
+	return tree
+
+if __name__ == "__main__":
+	L = []
+	eq = 0
+	total = 0
+	print "#"," ".join(sys.argv)
+	for line in open(sys.argv[1]):
+		line = line.strip()
+		if line == "":
+			total += 1
+			target = json.loads(L[3], object_hook=ascii_encode_dict)
+			target_DRSnode = DRSnode()
+			target_DRSnode.unserialization(target)
+
+			L[3] = tree(target_DRSnode)
+			normal_variables_for_trees(L[3])
+			L[3] = " ".join(L[3])
+			#normal_variables(target_DRSnode)
+
+			if L[3] != "DRS( )":
+				print "\n".join(L)
+				print 
+
+			L = []
+		else:
+			if line[0] == "#":
+				continue
+			L.append(line)
+	
+
+
+
+
---
+++ Tree/oracle/oracle.py
@@ -0,0 +1,41 @@
+import sys
+
+def printout(line, L):
+	assert line in L
+	print " ||| ".join(L[line])
+
+if __name__ == "__main__":
+	L = {}
+	tmpL = []
+	cnt = 0
+	total = 0
+	prev = ""
+	print "#", " ".join(sys.argv)
+	for line in open(sys.argv[1]):
+		line = line.strip()
+		if line == "":
+			if len(tmpL) == 0:
+				continue
+			if len(tmpL[3:]) == 0:
+				print tmpL[0]
+				exit()
+			L[tmpL[0]] = tmpL[3:]
+			tmpL = []
+		else:
+			if line[0] == "#":
+				continue
+			tmpL.append(line)
+
+	for line in open(sys.argv[2]):
+		line = line.strip()
+		if line[0] == "#":
+			continue
+		printout(line, L)
+
+		
+
+	#print cnt
+
+
+
+
