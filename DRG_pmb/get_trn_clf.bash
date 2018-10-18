afba03a9b17fb928ff63962dbf71db4d7d251d84
Jiangming Liu <jmliunlp@gmail.com>
2018-10-18 17:32:33 +0100

    add sense

--- DRG_pmb/get_trn_clf.bash
+++ DRG_pmb/get_trn_clf.bash
@@ -1,10 +1,11 @@
 
-cat data/silver/p0[2-9]/*/en.clf > tmp0
+#sense=.nosense
+cat data/silver/p0[2-9]/*/en.clf${sense} > tmp0
 
 for i in `seq 1 9`
 do
-cat data/silver/p${i}[0-9]/*/en.clf > tmp${i}
+cat data/silver/p${i}[0-9]/*/en.clf${sense} > tmp${i}
 done
 
-cat tmp* > trn.clf
+cat tmp* > trn.clf${sense}
 rm tmp*
