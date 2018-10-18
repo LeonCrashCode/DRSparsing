489bef073f27996eb49e8e485055a71ea770def1
Jiangming Liu <jmliunlp@gmail.com>
2018-10-18 17:33:30 +0100

    add sense

--- DRG_pmb/preprocessCLF.sh
+++ DRG_pmb/preprocessCLF.sh
@@ -4,6 +4,6 @@ do
 	for d in `ls -d ${1}/${f}/*`
 	do
 		echo ${d}
-		python preprocessCLF.py ${d}/en.drs.xml ${d}/en.drs.clf > ${d}/en.clf
+		python preprocessCLF.py ${d}/en.drs.xml ${d}/en.drs.clf 0 > ${d}/en.clf.nosense
 	done
 done
