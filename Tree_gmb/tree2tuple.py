ff21965d2107a501637563545153a06a77df574d
Jiangming <jmliunlp@gmail.com>
2018-11-10 15:53:22 +0000

    add

--- Tree_gmb/tree2tuple.py
+++ Tree_gmb/tree2tuple.py
@@ -1,7 +1,16 @@
 import sys
 import re
+import argparse
 
-special = ["NOT(", "POS(", "NEC(", "OR(", "IMP(", "DUP("]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", required=True)
+parser.add_argument("--novar", action='store_true')
+parser.add_argument("--norel", action='store_true')
+parser.add_argument("--partial", action='store_true')
+args = parser.parse_args()
+
+special = ["NOT(", "POS(", "NEC(", "OR(","IMP(", "DUP("]
 
 
 def get_b(stack):
@@ -9,7 +18,7 @@ def get_b(stack):
 		if item[0] == "b":
 			return item
 
-def process(tokens, partially):
+def process(tokens):
 	
 	i = 0
 	b = 0
@@ -33,7 +42,7 @@ def process(tokens, partially):
 	i = 0
 	k2b = {}
 	while i < len(tokens):
-		if re.match("^K[0-9]\($", tokens[i]):
+		if re.match("^K[0-9]+\($", tokens[i]):
 			assert tokens[i+1] in ["DRS(", "SDRS("]
 			k2b[tokens[i][:-1]] = "b"+b_n[i+1]
 		i += 1
@@ -41,6 +50,8 @@ def process(tokens, partially):
 	i = 0
 	stack = []
 	tuples = []
+	if args.norel:
+		tuples.append(["w0", "ROOT", "b0"])
 	while i < len(tokens):
 		tok = tokens[i]
 		if tok in ["DRS(", "SDRS("]:
@@ -88,14 +99,27 @@ def process(tokens, partially):
 		else:
 			if tok == "Equ(":
 				tok = "EQU("
-			tuples.append([get_b(stack), tok[:-1]])
+			if args.norel:
+				pass
+			else:
+				tuples.append([get_b(stack), tok[:-1]])
 			#v1
 			i += 1
 			assert tokens[i] != ")"
 			if re.match("^K[0-9]+$", tokens[i]):
-				tuples[-1].append(k2b[tokens[i]].lower())
+				if args.norel:
+					pass
+				elif args.novar:
+					tuples[-1].append("x1")
+				else:
+					tuples[-1].append(k2b[tokens[i]].lower())
 			else:
-				tuples[-1].append(tokens[i].lower())
+				if args.norel:
+					pass
+				elif args.novar:
+					tuples[-1].append("x1")
+				else:
+					tuples[-1].append(tokens[i].lower())
 
 			#v2
 			i += 1
@@ -103,12 +127,21 @@ def process(tokens, partially):
 				i += 1
 			else:
 				if tokens[i].startswith("CARD_NUMBER") or tokens[i].startswith("TIME_NUMBER"):
-					tuples[-1].append('"'+tokens[i]+'"')
+					if args.norel or args.novar:
+						pass
+					else:
+						tuples[-1].append('"'+tokens[i]+'"')
 				else:
 					if re.match("^K[0-9]+$", tokens[i]):
-						tuples[-1].append(k2b[tokens[i]].lower())
+						if args.norel or args.novar:
+							pass
+						else:
+							tuples[-1].append(k2b[tokens[i]].lower())
 					else:
-						tuples[-1].append(tokens[i].lower())
+						if args.norel or args.novar:
+							pass
+						else:
+							tuples[-1].append(tokens[i].lower())
 
 					#v at most 2
 				i += 1
@@ -117,7 +150,7 @@ def process(tokens, partially):
 				i += 1
 	assert len(tuples)!=0
 
-	if partially:
+	if args.partial and args.novar == False and args.norel == False:
 		c = 0
 		for item in tuples:
 			if item[1] in ["NOT", "POS", "NEC", "OR", "IMP", "DUP", "DRS"]:
@@ -135,10 +168,7 @@ def process(tokens, partially):
 		for item in tuples:
 			print " ".join(item)
 		print
-for line in open(sys.argv[1]):
+for line in open(args.input):
 	line = line.strip()
 	assert line[:5] == "SDRS(" or line[:4] == "DRS("
-	if len(sys.argv) >= 3 and sys.argv[2] == "partially":
-		process(line.split(), True)
-	else:
-		process(line.split(), False)
+	process(line.split())
