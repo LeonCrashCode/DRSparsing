0d347e483e6ae0032d8d3c99536eedb41d554888
Jiangming Liu <jmliunlp@gmail.com>
2018-11-14 15:04:15 +0000

    add interprete

--- Tree_gmb/interpretable.py
+++ Tree_gmb/interpretable.py
@@ -1,97 +1,29 @@
-def illegal_struct(struct):
-	if struct[0] not in ["DRS(" ,"SDRS("]:
-		return True, "root error"
-	for item in struct:
-		if item in ["DRS(", "SDRS(", "NOT(", "NEC(", "POS(", "IMP(", "OR(", "DUP(", ")"]:
-			continue
-		if re.match("^[PK][0-9]+\($", item):
-			continue
-		return True, "label error"
-	cnt = 0
-	for item in struct:
-		if item == ")":
-			cnt -= 1
-		else:
-			cnt += 1
-	if cnt != 0:
-		return True, "bracket error"
-
-	for i in range(len(struct)):
-		if re.match("^[PK][0-9]+\($", struct[i]):
-			if i + 1 < len(struct) and (struct[i+1] in ["DRS(", "SDRS("]):
-				continue
-			else:
-				return True, "PK should have box"
-	
-	stack = []
-	for item in struct:
-		if item[-1] == "(":
-			stack.append([item[-1],0])
-		else:
-			b = stack[-1]
-			stack.pop()
-			if re.match("^P[0-9]+\($", b[0]):
-				if stack[-1][0] == "DRS(":
-					pass
-				else:
-					return True, "P should be in DRS"
-			if re.match("^K[0-9]+\($", b[0]):
-				if stack[-1][0] == "SDRS(":
-                                        pass
-                                else:
-                                        return True, "K should be in SDRS"
-				stack[-1][1] += 1
-			if b[0] == "SDRS(":
-				if b[1] >= 2:
-					pass
-				else:
-					return True, "SDRS should have at least two segments"
-	return False, "no message"
-
-def illegal_rel(actn_v, rel):
-	for item in rel:
-		a = ""
-		if item >= actn_v.size():
-			a = "dummy("
-		else:
-			a = actn_v.totok(item)
-		if a[-1] != "(" and a != ")":
-			return True, "should be relation"
-	if rel[-1] < actn_v.size() and actn_v.totok(rel[-1]) == ")":
-		pass
-	else:
-		return True, "relation loop"
-	return False, "no message"
-
-def illegal_var(actn_v, rel, var):
-	for item in var:
-		a = actn_v.totok(item)
-		if a == ")":
-			pass
-		elif a in ["CARD_NUMBER", "TIME_NUMBER"]:
-			pass
-		elif re.match("^[XESTPK][0-9]+$",a):
-			pass
-		else:
-			return True, "should be variable"
-	if len(var) != 2 and len(var) != 3:
-		return True, "should have one or two variable"
-	if var[-1] < actn_v.size() and actn_v.totok(var[-1]) == ")":
-		pass
-	else:
-		return True, "variable loop"
-	
-	cansame = False
-	
-	if rel < actn_v.size() and actn_v.totok(rel) == "Equ(":
-		cansame = True
-	if cansame == False and len(var) == 3 and var[0] == var[1]:
-		return True, "relation semantic loop"
-	return False, "no message"
-
+import sys
+import re
+#tool function
+def is_p_scope(tok):
+	return re.match("^P[0-9]+\($",tok)
+def is_k_scope(tok):
+	return re.match("^K[0-9]+\($",tok)
+def is_relation(tok):
+	if tok[-1] != "(":
+		return False
+	if tok in ["DRS(", "SDRS(", "NOT(", "NEC(", "POS(", "IMP(", "OR(", "DUP("]:
+		return False
+	if is_p_scope(tok):
+		return False
+	if is_k_scope(tok):
+		return False
+	return True
+def is_variable(tok):
+	if re.match("^[XESTPK][0-9]+$", tok):
+		return True
+	if tok in ["CARD_NUMBER", "TIME_NUMBER"]:
+		return True
+	return False
 
 #structure
-def bracket_legal(tree):
+def bracket(tree):
 	# should be a tree
 	cnt = 0
 	for item in tree:
@@ -100,10 +32,31 @@ def bracket_legal(tree):
 		elif item == ")":
 			cnt -= 1
 	return cnt == 0
-def root_legal(tree):
+def root(tree):
 	# root should be DRS or SDRS
 	return tree[0] in ["DRS(", "SDRS("]
-
+def recursive(tree):
+	# structure should be recursively constructed
+	stack = []
+	for n in tree:
+		#print n
+		if n[-1] == "(":
+			stack.append(n)
+		elif n == ")":
+			b = stack.pop()
+			if len(stack) == 0:
+				continue
+			if b == "DRS(" or b == "SDRS(":
+				if stack[-1] in ["NOT(", "NEC(", "POS(", "IMP(", "OR(", "DUP("]:
+					pass
+				elif is_p_scope(stack[-1]) or is_k_scope(stack[-1]):
+					pass
+				else:
+					return False
+			else:
+				if stack[-1] != "DRS(" and stack[-1] != "SDRS(":
+					return False
+	return True
 def pk(tree):
 	# P( and K( should have only one box to represent its meaning
 	stack = []
@@ -111,27 +64,35 @@ def pk(tree):
 		if n[-1] == "(":
 			stack.append([n,0])
 		elif n == ")":
-			b = stack[-1]
-			stack.pop()
-			if re.match("^[PK][0-9]+\($",stack[-1][0]):
+			b = stack.pop()
+			if len(stack) == 0:
+				continue
+			if is_p_scope(stack[-1][0]) or is_k_scope(stack[-1][0]):
 				if b[0] not in ["SDRS(", "DRS("]:
 					return False
-				stack[-1][0] += 1
-			elif re.match("^[PK][0-9]+\(", b[0]):
+				stack[-1][1] += 1
+			elif is_p_scope(b[0]) or is_k_scope(b[0]):
 				if b[1] != 1:
 					return False
 	return True
-
-def SDRSsegments(tree):
+def pk_only(tree):
+	# P( and K( scope's names should be identified.
+	scopes = {}
+	for n in tree:
+		if is_p_scope(n) or is_k_scope(n):
+			if n in scopes:
+				return False
+			scopes[n] = 1
+	return True
+def SDRSsegment(tree):
 	# SDRS should only have segments, and at least two segments.
 	stack = []
 	for n in tree:
 		if n[-1] == "(":
 			stack.append([n,0])
 		elif n == ")":
-			b = stack[-1]
-			stack.pop()
-			if stack[-1][0] == "SDRS(":
+			b = stack.pop()
+			if len(stack) != 0 and stack[-1][0] == "SDRS(":
 				if re.match("^K[0-9]+\($",b[0]):
 					stack[-1][1] += 1
 					pass
@@ -143,28 +104,12 @@ def SDRSsegments(tree):
 				if b[1] < 2:
 					return False
 	return True
-
 def NoEmpty(tree):
 	# there should no empty box, and relations which have no variables
 	for i in range(len(tree)-1):
 		if tree[i][-1] == "(" and tree[i+1] == ")":
 			return False
 	return True
-
-def is_relation(tok):
-	if tok[-1] != "(":
-		return False
-	if tol in ["DRS(", "SDRS(", "NOT(", "NEC(", "POS(", "IMP(", "OR(", "DUP("]:
-		return False
-	if re.match("^[PK][0-9]+\($",tok):
-		return False
-	return True
-def is_variable(tok):
-	if re.match("^[XESTPK][0-9]+$", tok):
-		return True
-	if tok in ["CARD_NUMBER", "TIME_NUMBER"]:
-		return True
-	return False
 def VariableCount(tree):
 	# relation should only have one or two variables
 	for i in range(len(tree)):
@@ -177,9 +122,7 @@ def VariableCount(tree):
 		else:
 			return False
 	return True
-
 #Semantics
-
 def NoRelationLoop(tree):
 	# should not produce relation unlimitedly 
 	for n in tree:
@@ -189,9 +132,8 @@ def NoRelationLoop(tree):
 				if n == nn:
 					cnt += 1
 			if cnt == 40:
-				return True
-	return False
-
+				return False
+	return True
 def NoSemanticLoop(tree):
 	# should not assign two same variables a relation except Equ()
 	for i in range(len(tree)):
@@ -203,7 +145,6 @@ def NoSemanticLoop(tree):
 			if tree[i+1] == tree[i+2]:
 				return False
 	return True
-
 def scopedSegments(tree):
 	# an SDRS only assign relations onto segments, which are scoped in current SDRS
 	stack = []
@@ -212,32 +153,67 @@ def scopedSegments(tree):
 	for n in tree:
 		if n[-1] == "(":
 			if n == "SDRS(":
-				stack.append([n, sdrs_idx, []])
+				stack.append([sdrs_idx, []])
 				sdrs_idx += 1
+			elif is_k_scope(n):
+				stack.append([1000+int(n[1:-1]), []])
 			else:
-				stack.append([n, -1, []])
+				stack.append([-1, []])
 		elif n == ")":
-			b = stack[-1]
-			stack.pop()
-			if stack[-1][0] == "SDRS(":
-				if re.match("^K[0-9]+\($",b[0]):
-					stack[-1][-1].append(b[0][:-1])
-			elif b[0] == "SDRS(":
-				k_scoped[b[1]] = b[2]
+			b = stack.pop()
+			if b[0] != -1 and b[0] < 1000:
+				k_scoped[b[0]] = b[1]
+			if len(stack) > 0:
+				stack[-1][1] = stack[-1][1] + b[1]
+			if len(stack) > 0 and b[0] >= 1000:
+				stack[-1][1].append("K"+str(b[0]%1000))
 
+	sdrs_idx = 0
 	for n in tree:
 		if n[-1] == "(":
-				stack.append([n, []])
+			if n == "SDRS(":
+				stack.append([sdrs_idx, []])
+				sdrs_idx += 1
+			else:
+				stack.append([-1, []])
 		elif n == ")":
-			b = stack[-1]
-			stack.pop()
-			if is_relation(stack[-1][0]):
-				if re.match("^K[0-9]+\($",b[0]):
-					stack[-1][-1].append(b[0][:-1])
-			elif b[0] == "SDRS(":
-				k_scoped[b[1]] = b[2]
+			b = stack.pop()
+			if len(stack) > 0 and stack[-1][0] != -1:
+				stack[-1][1] = stack[-1][1] + b[1]
+			if b[0] != -1:
+				for k in b[1]:
+					if k not in k_scoped[b[0]]:
+						return False
+		else:
+			stack[-1][1].append(n)
 	return True
 
-
 if __name__ == "__main__":
-	#structure
+	n = 0
+
+	fun1 = [bracket, root, recursive, pk, pk_only, SDRSsegment, NoEmpty, VariableCount]
+	fun2 = [NoRelationLoop, NoSemanticLoop, scopedSegments]
+
+	count1 = [0 for i in range(len(fun1))]
+	count2 = [0 for i in range(len(fun2))]
+	for line in open(sys.argv[1]):
+		line = line.strip()
+		if line == "":
+			continue
+		line = line.split()
+		print n
+		n += 1
+
+		for i, fun in enumerate(fun1):
+			if fun(line) == False:
+				count1[i] += 1
+		for i, fun in enumerate(fun2):
+			if fun(line) == False:
+				count2[i] += 1
+
+	for i, err in enumerate(count1):
+		print fun1[i].__name__, ":", err
+
+	for i, err in enumerate(count2):
+		print fun2[i].__name__, ":", err
+
