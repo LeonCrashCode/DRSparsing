014bc0a6d8d738abadf28c47a0c650b5ebb32dd5
Jiangming Liu <jmliunlp@gmail.com>
2018-12-26 19:44:31 +0000

    update

--- Tree_gmb/tree2oracle_tcn.py
+++ Tree_gmb/tree2oracle_tcn.py
@@ -2,7 +2,7 @@ import os
 import sys
 import re
 
-v_p = re.compile("^[XESTPK][0-9]+$")
+v_p = re.compile("^[XESTPKB][0-9]+$")
 d_p = re.compile("^DRS-[0-9]+\($")
 pb = re.compile("^P[0-9]+\($")
 kb = re.compile("^K[0-9]+\($")
@@ -34,12 +34,106 @@ def bracket2list(bracket):
 	return stack[0]
 
 def tree2oracle(tree, out_action):
+	v = ["X","E","S","T","B","P","K"]
+	vl = [ [] for i in range(7)]
+
 	for i in range(len(tree)):
-		if pb.match(tree[i]):
-			tree[i] = "@P("
-		elif kb.match(tree[i]):
-			tree[i] = "@K("
+		tok = tree[i]
+		if pb.match(tok):
+			assert tok[:-1] not in vl[-2]
+			vl[-2].append(tok[:-1])
+			assert v_p.match(tree[i+1]) and tree[i+1][0] == "B"
+			if tree[i+1] != "B0" and tree[i+1] not in vl[-3]:
+				vl[-3].append(tree[i+1])
+		if kb.match(tok):
+			assert tok[:-1] not in vl[-1]
+			vl[-1].append(tok[:-1])
+		if tok in ["NOT(", "POS(", "NEC(", "IMP(", "OR(", "DUP("]:
+			assert v_p.match(tree[i+1]) and tree[i+1][0] == "B"
+			if tree[i+1] != "B0" and tree[i+1] not in vl[-3]:
+				vl[-3].append(tree[i+1])
+	#print vl
+	#exit(1)
+	root = bracket2list(tree)
+
+	def is_struct(tok):
+		if tok in ["DRS(", "SDRS(", "NOT(", "POS(", "NEC(", "IMP(", "OR(", "DUP("]:
+			return True
+		if d_p.match(tok):
+			return True
+		if re.match("^[PK][0-9]+\($", tok):
+			return True
+		return False
+
+	def travel(root):
+		#global v
+		#global vl
+		parent = root[0]
+		child = root[1:]
+		if parent == "SDRS(":
+			for c in child:
+				if not is_struct(c[0]):
+					for cc in c[1:]:
+						if v_p.match(cc):
+							idx = v.index(cc[0])
+							if cc not in vl[idx]:
+								vl[idx].append(cc)
+			for c in child:
+				if is_struct(c[0]):
+					travel(c)
+		elif parent == "DRS(" or d_p.match(parent):
+			for c in child:
+				if not is_struct(c[0]):
+					assert c[1][0] == "B"
+					assert v_p.match(c[1]) and c[1][0] == "B"
+					idx = v.index(c[1][0])
+					if c[1] not in vl[idx] and c[1] != "B0":
+						vl[idx].append(c[1])
+					for cc in c[2:]:
+						if v_p.match(cc):
+							idx = v.index(cc[0])
+							if cc not in vl[idx]:
+								vl[idx].append(cc)
+			for c in child:
+				if is_struct(c[0]):
+					travel(c)
+		elif pb.match(parent) or parent in ["NOT(", "POS(", "NEC(", "IMP(", "OR(", "DUP("]:
+			for c in child[1:]:
+				travel(c)
+		elif kb.match(parent):
+			for c in child:
+				travel(c)
+		
+	travel(root)
 	correct(tree)
+	i = 0
+	cur = 0
+	while i < len(tree):
+		tok = tree[i]
+		if pb.match(tok):
+			idx = vl[-2].index(tok[:-1])
+			tree[i] = "P"+str(idx+1)+"("
+		elif kb.match(tok):
+			idx = vl[-1].index(tok[:-1])
+			tree[i] = "K"+str(idx+1)+"("
+		elif v_p.match(tok) and tok != "B0":
+			vl_idx = v.index(tok[0])
+			idx = vl[vl_idx].index(tok)
+			tree[i] = v[vl_idx] + str(idx+1)
+		i += 1
+
+	p_n = 0
+	k_n = 0
+	for i in range(len(tree)):
+		tok = tree[i]
+		if pb.match(tok):
+			assert int(tok[1:-1]) == p_n + 1
+			tree[i] = "@P("
+			p_n += 1
+		if kb.match(tok):
+			assert int(tok[1:-1]) == k_n + 1
+			tree[i] = "@K("
+			k_n += 1
 	"""	
 	B = 1
 	for i in range(len(tree)):
