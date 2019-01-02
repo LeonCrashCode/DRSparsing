8a122cd974e9c1dfc1a24fd1c1dcb55973a14142
Jiangming Liu <jiangming_liu@JM-MacBook.local>
2019-01-02 17:26:29 +0000

    update

--- Tree_gmb/tree2ground_tcn.py
+++ Tree_gmb/tree2ground_tcn.py
@@ -7,9 +7,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--input", required=True)
-parser.add_argument("--novar", action='store_true')
-parser.add_argument("--norel", action='store_true')
-parser.add_argument("--partial", action='store_true')
+parser.add_argument("--timex", default='exp', help="exp, span, span_index, constant")
+parser.add_argument("--card", default='exp', help="exp, span, span_index, constant")
+parser.add_argument("--name", default='exp', help="exp, span, span_index")
+parser.add_argument("--pred", default='exp', help="exp, span, span_index")
 args = parser.parse_args()
 
 v_p = re.compile("^[XESTPKB][0-9]+$")
@@ -43,7 +44,7 @@ def bracket2list(bracket):
 	assert len(stack) == 1
 	return stack[0]
 
-def tree2oracle(tree, out_action):
+def tree2ground(tree):
 	v = ["X","E","S","T","B","P","K"]
 	vl = [ [] for i in range(7)]
 
@@ -147,62 +148,156 @@ def tree2oracle(tree, out_action):
 			assert int(tok[1:-1]) == k_n + 1
 			#tree[i] = "@K("
 			k_n += 1
-	
-	# keep index and remove [***]
+
+	print tree
 	n_tree = []
-	for i in range(len(tree)):
-		if tree[i][0] == "[" and tree[i][-1] == "]":
-			n_tree.append(tree[i][1:-1])
-		elif re.match("^\$[0-9]+\[.+\]\($", tree[i]):
-			idx = tree[i].index("[")
-			n_tree.append(tree[i][idx+1:-2]+"(")
-		elif re.match("^\$[0-9]+\[.+\]$", tree[i]): 	
-			idx = tree[i].index("[")
-			n_tree.append(tree[i][idx+1:-1])
-		elif re.match("^\$[0-9]+$", tree[i]):
-			pass
-		elif re.match("^T[yx][mx][dx]\($", tree[i]):
-			n_tree.append("Timex(")
+	stack = []
+	i = 0
+	while i < len(tree):
+		t = tree[i]
+		if v_p.match(t):
+			n_tree.append(t)
+			i += 1
+		elif t in ["DRS(", "SDRS("]:
+			n_tree.append(t)
+			stack.append(-1)
+			i += 1
+		elif d_p.match(t):
+			n_tree.append(t)
+			stack.append(int(t[4:-1])) #DRS-10(
+			i += 1
+		elif pb.match(t) or kb.match(t): 
+			n_tree.append(t)
+			stack.append(-1)
+			i += 1
+		elif t == ")":
+			n_tree.append(t)
+			stack.pop()
+			i += 1
+		elif t in ["NOT(", "POS(", "NEC(", "IMP(", "OR(", "DUP("]:
+			n_tree.append(t)
+			stack.append(-1)
+			i += 1
+		elif t == "Named(": #Named( B0 X1 $1[John] )
+			assert stack[-1] != -1
+			idx = tree[i:].index(")")
+			n_tree.append(t)
+			n_tree.append(tree[i+1])
+			n_tree.append(tree[i+2])
+
+			cons = []
+			for item in tree[i+3:i+idx]:
+				assert re.match("^\$[0-9]+\[.+\]$", item)
+				j = item.index("[")
+				if args.name == "exp":
+					cons.append(item[j+1:-1])
+				elif args.name == "span":
+					cons.append(words[stack[-1]][int(item[1:j])])
+				elif args.name == "span_index":
+					cons.append(item[:j])
+				else:
+					assert False, "unrecognized option for --name"
+			n_tree.append("~".join(cons))
+			n_tree.append(")")
+			i = i + idx + 1
+		elif t == "Card(":
+			assert stack[-1] != -1
+			idx = tree[i:].index(")")
+			n_tree.append(t)
+			n_tree.append(tree[i+1])
+			n_tree.append(tree[i+2])
+
+			indexs = []
+			exp = ""
+			for item in tree[i+3:i+idx-1]:
+				assert re.match("^\$[0-9]+$", item)
+				indexs.append(item)
+			assert tree[i+idx-1][0] == "[" and tree[i+idx-1][-1] == "]"
+			exp = tree[i+idx-1][1:-1]
+
+			if args.card == "exp":
+				n_tree.append(exp)
+			elif args.card == "span_index":
+				n_tree.append("~".join(indexs))
+			elif args.card == "span":
+				cons = []
+				for index in indexs:
+					cons.append(words[stack[-1]][int(index[1:])])
+				n_tree.append("~".join(cons))
+			elif args.card == "constant":
+				n_tree.append("CARD_NUMBER")
+			else:
+				assert False, "unrecognized option for --card"
+			n_tree.append(")")
+			i = i + idx + 1
+		elif re.match("^T[yx][mx][dx]\($", t):
+			assert stack[-1] != -1
+			idx = tree[i:].index(")")
+			n_tree.append(t)
+			n_tree.append(tree[i+1])
+			n_tree.append(tree[i+2])
+
+			indexs = []
+			exp = ""
+			for item in tree[i+3:i+idx-1]:
+				assert re.match("^\$[0-9]+$", item)
+				indexs.append(item)
+			assert tree[i+idx-1][0] == "[" and tree[i+idx-1][-1] == "]"
+			exp = tree[i+idx-1][1:-1]
+
+			if args.timex == "exp":
+				n_tree.append(exp)
+			elif args.timex == "span_index":
+				n_tree.append("~".join(indexs))
+			elif args.timex == "span":
+				cons = []
+				for index in indexs:
+					cons.append(words[stack[-1]][int(index[1:])])
+				n_tree.append("~".join(cons))
+			elif args.timex == "constant":
+				n_tree.append("TIME_NUMBER")
+			else:
+				assert False, "unrecognized option for --timex"
+			n_tree.append(")")
+			i = i + idx + 1
+		elif t[-1] == "(":
+			idx = tree[i:].index(")")
+			#Rel( B0 X1 X2 )
+			if idx == 4 and all([v_p.match(x) for x in tree[i+1:i+idx]]):
+				assert stack[-1] != -1
+				if re.match("^\$[0-9]+\[.+\]\($", t):
+					j = t.index("[")
+					n_tree.append(t[j+1:-2]+"(")
+				else:
+					n_tree.append(t)
+				n_tree += tree[i+1:i+idx]
+			#Rel( B0 X1 n.00 )
+			elif idx == 4 and all([v_p.match(x) for x in tree[i+1:i+idx-1]]) and re.match("^[anvr]\.[0-9][0-9]$", tree[i+idx-1]):
+				assert stack[-1] != -1
+				if re.match("^\$[0-9]+\[.+\]\($", t):
+					j = t.index("[")
+					if args.pred == "exp":
+						n_tree.append(t[j+1:-2]+"(")
+					elif args.pred == "span":
+						n_tree.append(words[stack[-1]][int(t[1:j])]+"(")
+					elif args.pred == "span_index":
+						n_tree.append(t[:j]+"(")
+					else:
+						assert False, "unrecognized option for --pred"
+				n_tree += tree[i+1:i+idx]
+			#CONTINUATION( K1 K2 )
+			elif idx == 3 and all([v_p.match(x) for x in tree[i+1:i+idx]]):
+				assert t.isupper()
+				n_tree += tree[i:i+idx]
+			else:
+				assert False, "unrecognized format"
+
+			n_tree.append(")")
+
+			i = i + idx + 1
 		else:
-			n_tree.append(tree[i])
-	out_action.write(" ".join(n_tree)+"\n")
-	"""	
-	B = 1
-	for i in range(len(tree)):
-		if tree[i] in ["@P(", "POS(", "NEC(", "NOT(", "IMP(", "OR(", "DUP("]:
-			if tree[i+1] == "B"+str(B):
-				tree[i+1] = "@B"
-				B += 1
-	X = E = S = T = 1
-	for i in range(len(tree)):
-		if re.match("^X[0-9]+$", tree[i]):
-			assert int(tree[i][1:]) <= X
-			if tree[i] == "X"+str(X):
-				tree[i] = "@X"
-				X += 1
-		elif re.match("^E[0-9]+$", tree[i]):
-			assert int(tree[i][1:]) <= E
-			if tree[i] == "E"+str(E):
-				tree[i] = "@E"
-				E += 1
-		elif re.match("^S[0-9]+$", tree[i]):
-                        assert int(tree[i][1:]) <= S
-                        if tree[i] == "S"+str(S):
-                                tree[i] = "@S"
-                                S += 1
-		elif re.match("^T[0-9]+$", tree[i]):
-                        assert int(tree[i][1:]) <= T
-                        if tree[i] == "T"+str(T):
-                                tree[i] = "@T"
-                                T += 1
-		elif re.match("^B[0-9]+$", tree[i]):
-                        assert int(tree[i][1:]) <= B
-                        if tree[i] == "B"+str(B):
-                                tree[i] = "@B"
-                                B += 1
-	"""
-	#out_action.write(" ".join(tree)+"\n")	
-		
+			assert False, "unrecognized format"
+	print " ".join(n_tree)
 
 def filter(illform, tree):
 	#filter two long sentences, actually only one
@@ -222,48 +317,34 @@ def filter(illform, tree):
 if __name__ == "__main__":
 	
 	illform = []
-	"""
-	for line in open("illform"):
-		line = line.strip()
-		if line == "":
-			continue
-		illform.append(line.split()[-2])
-	"""
 	if os.path.exists("manual_correct2"):
 		for line in open("manual_correct2"):
 			line = line.strip()
 			if line == "" or line[0] == "#":
 				continue
 			illform.append(line.split()[0])
-	
+
+	global words
 	lines = []
 	filename = ""
-	#out_input = open(sys.argv[1]+".oracle.in", "w")
-	out_action = open(sys.argv[1]+".ground", "w")
-	for line in open(sys.argv[1]):
+	for line in open(args.input):
 		line = line.strip()
 		if line == "":
-
 			idx = lines.index("TREE")
-			
-			words = " ||| ".join(lines[:idx])
-
+			words = lines[:idx]
+			for i in range(len(words)):
+				words[i] = words[i].split()
 			tree = lines[idx+1].split()
 			if filter(illform, tree):
 				lines = []
 				continue
-
-			#out_input.write(words + "\n\n")
-			tree2oracle(tree, out_action)
-			#out_input.flush()
-			out_action.flush()
+			tree2ground(tree)
 			lines = []
 		else:
 			if line[0] == "#":
 				filename = line.split()[-1]
 				continue
 			lines.append(line)
-	#out_input.close()
-	out_action.close()
+
 
 			
