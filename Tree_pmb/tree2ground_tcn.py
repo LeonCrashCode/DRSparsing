4f4150e1c8c368dbedfe745553b58297b5fbb9b2
Jiangming Liu <jiangming_liu@JM-MacBook.local>
2019-01-14 18:45:53 +0000

    update

--- Tree_pmb/tree2ground_tcn.py
+++ Tree_pmb/tree2ground_tcn.py
@@ -6,9 +6,9 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--input", required=True)
-parser.add_argument("--type", default="exp", help="exp | span | span_index"
-parser.add_argument("--connect", default='~', help="it connects two spans, default is ~")
+parser.add_argument("-i", "--input", required=True)
+parser.add_argument("-t", "--type", default="exp", help="exp | span | span_index")
+parser.add_argument("-c", "--connect", default='~', help="it connects two spans, default is ~")
 args = parser.parse_args()
 
 v_p = re.compile("^[XESTPKB][0-9]+$")
@@ -16,149 +16,22 @@ d_p = re.compile("^DRS-[0-9]+\($")
 pb = re.compile("^P[0-9]+\($")
 kb = re.compile("^K[0-9]+\($")
 
-def correct(tree):
-	# Here we correct some weired things
+def is_variable(v):
+	if v_p.match(v):
+		return True
+	if v in ["\"speaker\"", "\"hearer\"", "\"now\"", "\"?\""]:
+		return True
+	return False
 
-	#e.g. :( K1 K2 )
-	for i in range(len(tree)):
-		if tree[i] == ":(" and tree[i+1][0] == "K":
-			tree[i] = "THAT("
-		if tree[i] == "-(" and tree[i+1][0] == "K":
-			tree[i] = "THAT("
-		if tree[i] == "((" and tree[i+1][0] == "K":
-			tree[i] = "THAT("
-
-def bracket2list(bracket):
-	stack = []
-	for tok in bracket:
-		if tok[-1] == "(":
-			stack.append([tok])
-		elif tok == ")":
-			if len(stack) != 1:
-				back = stack.pop()
-				stack[-1].append(back)
-		else:
-			stack[-1].append(tok)
-	assert len(stack) == 1
-	return stack[0]
-
-def predicate(t):
-	index = []
-	exp = []
-	bvs = []
-	
-	i = 0
-	while True:
-		if ")" not in t:
-			break
-		if not re.match("^\$[0-9]+\[.+\]\($",t[i]):
-			break
-		idx = t.index(")")
-			
-def tree2ground(tree):
-	v = ["X","E","S","T","B","P","K"]
-	vl = [ [] for i in range(7)]
-
-	for i in range(len(tree)):
-		tok = tree[i]
-		if pb.match(tok):
-			assert tok[:-1] not in vl[-2]
-			vl[-2].append(tok[:-1])
-			assert v_p.match(tree[i+1]) and tree[i+1][0] == "B"
-			if tree[i+1] != "B0" and tree[i+1] not in vl[-3]:
-				vl[-3].append(tree[i+1])
-		if kb.match(tok):
-			assert tok[:-1] not in vl[-1]
-			vl[-1].append(tok[:-1])
-		if tok in ["NOT(", "POS(", "NEC(", "IMP(", "OR(", "DUP("]:
-			assert v_p.match(tree[i+1]) and tree[i+1][0] == "B"
-			if tree[i+1] != "B0" and tree[i+1] not in vl[-3]:
-				vl[-3].append(tree[i+1])
-	#print vl
-	#exit(1)
-	root = bracket2list(tree)
-
-	def is_struct(tok):
-		if tok in ["DRS(", "SDRS(", "NOT(", "POS(", "NEC(", "IMP(", "OR(", "DUP("]:
-			return True
-		if d_p.match(tok):
-			return True
-		if re.match("^[PK][0-9]+\($", tok):
-			return True
+def lookahead(t, b, v, s):
+	if len(t) < 4:
 		return False
 
-	def travel(root):
-		#global v
-		#global vl
-		parent = root[0]
-		child = root[1:]
-		if parent == "SDRS(":
-			for c in child:
-				if not is_struct(c[0]):
-					for cc in c[1:]:
-						if v_p.match(cc):
-							idx = v.index(cc[0])
-							if cc not in vl[idx]:
-								vl[idx].append(cc)
-			for c in child:
-				if is_struct(c[0]):
-					travel(c)
-		elif parent == "DRS(" or d_p.match(parent):
-			for c in child:
-				if not is_struct(c[0]):
-					assert c[1][0] == "B"
-					assert v_p.match(c[1]) and c[1][0] == "B"
-					idx = v.index(c[1][0])
-					if c[1] not in vl[idx] and c[1] != "B0":
-						vl[idx].append(c[1])
-					for cc in c[2:]:
-						if v_p.match(cc):
-							idx = v.index(cc[0])
-							if cc not in vl[idx]:
-								vl[idx].append(cc)
-			for c in child:
-				if is_struct(c[0]):
-					travel(c)
-		elif pb.match(parent) or parent in ["NOT(", "POS(", "NEC(", "IMP(", "OR(", "DUP("]:
-			for c in child[1:]:
-				travel(c)
-		elif kb.match(parent):
-			for c in child:
-				travel(c)
-		
-	travel(root)
-	correct(tree)
-
-	# normalize variables
-	i = 0
-	cur = 0
-	while i < len(tree):
-		tok = tree[i]
-		if pb.match(tok):
-			idx = vl[-2].index(tok[:-1])
-			tree[i] = "P"+str(idx+1)+"("
-		elif kb.match(tok):
-			idx = vl[-1].index(tok[:-1])
-			tree[i] = "K"+str(idx+1)+"("
-		elif v_p.match(tok) and tok != "B0":
-			vl_idx = v.index(tok[0])
-			idx = vl[vl_idx].index(tok)
-			tree[i] = v[vl_idx] + str(idx+1)
-		i += 1
-
-	# normalize scoped K and P
-	p_n = 0
-	k_n = 0
-	for i in range(len(tree)):
-		tok = tree[i]
-		if pb.match(tok):
-			assert int(tok[1:-1]) == p_n + 1
-			#tree[i] = "@P("
-			p_n += 1
-		if kb.match(tok):
-			assert int(tok[1:-1]) == k_n + 1
-			#tree[i] = "@K("
-			k_n += 1
+	if re.match("^\$[0-9]+\[.+\]\($",t[0]) and t[1] == b and t[2] == v and t[3] == s:
+		return True
+	return False
+			
+def tree2ground(tree):
 
 	#print tree
 	n_tree = []
@@ -166,7 +39,7 @@ def tree2ground(tree):
 	i = 0
 	while i < len(tree):
 		t = tree[i]
-		if v_p.match(t):
+		if re.match("^B[0-9]+$", t):
 			n_tree.append(t)
 			i += 1
 		elif t in ["DRS(", "SDRS("]:
@@ -189,7 +62,8 @@ def tree2ground(tree):
 			n_tree.append(t)
 			stack.append(-1)
 			i += 1
-		elif t[0:5]== "Named" and t[-1] == "(": #Named( B0 X1 $1 [John] )
+		elif t[0:5]== "Named" and t[-1] == "(":
+			#Named( B0 X1 $1 [John] )
 			assert stack[-1] != -1
 			idx = tree[i:].index(")")
 			n_tree.append(t)
@@ -202,29 +76,61 @@ def tree2ground(tree):
 				assert re.match("^\$[0-9]+$", item)
 				indexs.append(item)
 			assert tree[i+idx-1][0] == "[" and tree[i+idx-1][-1] == "]"
-                        exp = tree[i+idx-1][1:-1]
+			exp = tree[i+idx-1][1:-1]
 
 			if args.type == "exp":
-				n_tree.append(exp)
+				n_tree.append('\"'+exp+'\"')
 			elif args.type == "span_index":
-                                n_tree.append(args.name_span_connect.join(indexs))
-                        elif args.type == "span":
-                                cons = []
-                                for index in indexs:
-                                        cons.append(words[stack[-1]][int(index[1:])])
-                                n_tree.append(args.name_span_connect.join(cons))
+				n_tree.append('\"'+args.connect.join(indexs)+'\"')
+			elif args.type == "span":
+				cons = []
+				for index in indexs:
+					cons.append('\"'+words[stack[-1]][int(index[1:])]+'\"')
+				n_tree.append(args.connect.join(cons))
 			else:
-				assert False, "unrecognized option for --name"
+				assert False, "unrecognized option for --type"
 			n_tree.append(")")
 			i = i + idx + 1
-		elif re.match("^\$[0-9]+\[.+\]\($",t):
-			#$0[rel0]( B0 X1 ... )
-			idx, pred = predicate(tree[i:])
-			n_tree += pred
+		elif t[-1] == "(" and re.match("[anvr]\.[0-9]{2}", tree[i+3]):
+			idx = tree[i:].index(")")
+			if re.match("^\$[0-9]+\[.+\]\($",t):
+				# $0[rel0]( B0 X1 sense )
+				j = t.index("[")
+				indexs = [t[0:j]]
+				exps = [t[j+1:-2]]
+				while lookahead(tree[i+idx+1:], tree[i+1], tree[i+2], tree[i+3]):
+					t = tree[idx+1]
+					j = t.index("[")
+					indexs.append(t[0:j])
+					exps.append(t[j+1:-2])
+					idx += tree[i+idx+1:].index(")")
+				if args.type == "exp":
+					n_tree.append(args.connect.join(exps)+"(")
+				elif args.type == "span_index":
+					n_tree.append(args.connect.join(indexs)+"(")
+				elif args.type == "span":
+					cons = []
+					for index in indexs:
+						cons.append(words[stack[-1]][int(index[1:])])
+					n_tree.append(args.name_span_connect.join(cons)+"(")
+				else:
+					assert False, "unrecognized option for --type"
+				n_tree.append(tree[i+1])
+				n_tree.append(tree[i+2])
+				n_tree.append(tree[i+3])
+				n_tree.append(")")
+			else:
+				# pred( B0 X1 sense )
+				n_tree.append(tree[i])
+				n_tree.append(tree[i+1])
+				n_tree.append(tree[i+2])
+				n_tree.append(tree[i+3])
+				n_tree.append(")")
 			i = i + idx + 1
 
 		elif t[-1] == "(":
 			idx = tree[i:].index(")")
+			#print tree[i:i+idx+1]
 			#Rel( B0 X1 X2 )
 			if idx == 4 and all([is_variable(x) for x in tree[i+1:i+idx]]):
 				assert stack[-1] != -1
@@ -234,20 +140,35 @@ def tree2ground(tree):
 				else:
 					n_tree.append(t)
 				n_tree += tree[i+1:i+idx]
-			#Rel( B0 X1 n.00 )
-			elif idx == 4 and all([v_p.match(x) for x in tree[i+1:i+idx-1]]) and re.match("^[anvr]\.[0-9][0-9]$", tree[i+idx-1]):
+			#Rel( B0 X1 ... )
+			elif idx == 4 and all([is_variable(x) for x in tree[i+1:i+3]]):
 				assert stack[-1] != -1
 				if re.match("^\$[0-9]+\[.+\]\($", t):
 					j = t.index("[")
-					if args.pred == "exp":
-						n_tree.append(t[j+1:-2]+"(")
-					elif args.pred == "span":
-						n_tree.append(words[stack[-1]][int(t[1:j])]+"(")
-					elif args.pred == "span_index":
-						n_tree.append(t[:j]+"(")
+					n_tree.append(t[j+1:-2]+"(")
+				else:
+					n_tree.append(t)
+				n_tree += tree[i+1:i+3]
+				
+				indexs = []
+				exps = []
+				for item in tree[i+3:]:
+					if re.match("^\$[0-9]+$", item):
+						indexs.append(item)
+					elif item[0] == "[" and item[-1] == "]":
+						exps.append(item)
 					else:
-						assert False, "unrecognized option for --pred"
-				n_tree += tree[i+1:i+idx]
+						assert False, "unrecognized format"
+
+				if args.type == "exp":
+					n_tree.append(args.connect.join(exps))
+				elif args.type == "span_index":
+					n_tree.append(args.connect.join(indexs))
+				elif args.type == "span":
+					cons = []
+					for index in indexs:
+						cons.append(words[stack[-1]][int(index[1:])])
+					n_tree.append(args.name_span_connect.join(cons))
 			#CONTINUATION( K1 K2 )
 			elif idx == 3 and all([v_p.match(x) for x in tree[i+1:i+idx]]):
 				assert t.isupper()
@@ -262,30 +183,9 @@ def tree2ground(tree):
 			assert False, "unrecognized format"
 	print " ".join(n_tree)
 
-def filter(illform, tree):
-	#filter two long sentences, actually only one
-	"""
-	cnt = 0
-	for item in tree:
-		if item == "DRS(":
-			cnt += 1
-	if cnt >= 21:
-		return True
-	"""
-	for item in illform:
-		if item in tree:
-			return True
-	return False
 
 if __name__ == "__main__":
 	
-	illform = []
-	if os.path.exists("manual_correct2"):
-		for line in open("manual_correct2"):
-			line = line.strip()
-			if line == "" or line[0] == "#":
-				continue
-			illform.append(line.split()[0])
 
 	global words
 	lines = []
@@ -298,9 +198,6 @@ if __name__ == "__main__":
 			for i in range(len(words)):
 				words[i] = words[i].split()
 			tree = lines[idx+1].split()
-			if filter(illform, tree):
-				lines = []
-				continue
 			tree2ground(tree)
 			lines = []
 		else:
